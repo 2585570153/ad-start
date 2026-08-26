8ba2ded3f870f715c3c5b78a667ef476ff9fcb46
a.d <ad@aiheadn.cn>
2026-08-26 21:08:57 +0800

    切换ai

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,11 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+	readonly HEADER_INJECT?: string;
+	readonly FOOTER_INJECT?: string;
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
